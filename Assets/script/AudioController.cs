da7653112b20c37c99fd6f8ad45f3317a8d80071
Amgad Morsy <42896629+amgadkamal@users.noreply.github.com>
2020-03-31 23:57:27 -0600

    HW10.1

---
+++ Assets/script/AudioController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class AudioController : MonoBehaviour
+{
+  
+    
+
+    public AudioMixerSnapshot exploring;
+    public  AudioMixerSnapshot battling;
+    public AudioSource stingSound;
+    public AudioSource lose;
+    private float transitionTime = 1f;
+    
+    
+    
+
+ 
+
+    private void OnTriggerEnter2D(Collider2D collision)
+
+    {
+        if (collision.tag == "BattleZone")
+        {
+            battling.TransitionTo(transitionTime);
+            stingSound.Play();
+        }
+    }
+    
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+
+    { 
+        string collideTag = collision.gameObject.tag;
+        if (collideTag == "lose") 
+            //play lose track
+        {battling.TransitionTo(transitionTime);
+            lose.Play();
+        }
+    }
+    
+
+
+    private void OnTriggerExit(Collider2D collision)
+    {
+        if (collision.tag == "BattleZone")
+        { exploring.TransitionTo(transitionTime); }
+        
+        if (collision.tag == "lose")
+        {
+            exploring.TransitionTo(transitionTime); }
+    }
+
+
+}
---
+++ Assets/script/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+   //spawnpoint location
+    [SerializeField]
+    private GameObject spawnPoint ;
+    
+    //lsoe message
+    [SerializeField]
+    private GameObject loseimage;
+    
+    private float speed = 10f;
+    private Rigidbody2D rb;
+    
+    //bool used with collision to make (respawn & lose message) only happen after collision with "lose" game object 
+    private bool losing; 
+    Vector3 loseposition = new Vector3(0,0,0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(move * speed, rb.velocity.y);
+        
+        //mouse click to (spawn,destroy lose message, reset losing bool to false again after being true with collision.  
+        if (Input.GetMouseButtonDown(0) & (losing.Equals(true)))
+        {
+            this.gameObject.transform.SetPositionAndRotation(spawnPoint.transform.position, Quaternion.identity);
+            losing = false;
+            Destroy(GameObject.FindWithTag("loselose"));
+            
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+
+    {
+        string collideTag = collision.gameObject.tag;
+        if (collideTag == "lose") // if Mr Bean hits enemies, they will be destroyed and number of killed enemies increased by 1.
+        {
+            losing = true;
+            Instantiate(loseimage, loseposition, Quaternion.identity);
+        }
+    }
+
+   
+}
+
+
+
+
+
+
+
+
+
